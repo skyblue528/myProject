5ee2baa98a991d6a01a13a20b27137d4d33ddc7e
Julia Kyaw <skyblue.528@gmail.com>
2015-07-29 16:37:05 -0700

    fixed ttt game

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
-var app = express();
+var app = express();  // assign the return variable into variable call app
 
 app.set('port', (process.env.PORT || 5000));
+//console.log("process.env.PORT: " , process.env.PORT);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,7 +11,19 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  response.render('pages/home');
+});
+
+app.get('/index', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/learnMore', function(request, response) {
+  response.render('pages/tutorial');
 });
 
 app.listen(app.get('port'), function() {
