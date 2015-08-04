5ac704f77927270d64ec0114141fa6b782980223
Julia Kyaw <skyblue.528@gmail.com>
2015-08-04 16:19:05 -0700

    broken photoes click button

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var multer  = require('multer');
+var mv = require('mv');
+var cp =require('cp');
 var app = express();  // assign the return variable into variable call app
 var done = false;
-
+var filePath;
+var total=0;
 app.use(multer({ dest: './uploads/',
   rename: function (fieldname, filename) {
     return filename+Date.now();
@@ -11,7 +14,8 @@ app.use(multer({ dest: './uploads/',
     console.log(file.originalname + ' is starting ...')
   },
   onFileUploadComplete: function (file) {
-    console.log(file.fieldname + ' uploaded to  ' + file.path)
+    filePath = file.path;
+    console.log(file.fieldname + ' uploaded to  ' +filePath)
     done=true;
   }
 }));
@@ -44,19 +48,71 @@ app.get('/learnMore', function(request, response) {
 
 app.get('/profile',function(request, response){
   //res.sendfile('pages/profile');
+  console.log('request.query.name: ' +request.query.name);
+  if(request.query.name) {
+    console.log('request.query.name2: ' + request.query.name);
+    console.log("filePath: " + filePath)
+    filePath = 'public/images/' + request.query.name;
+
+    if (total++ % 2 == 0) {
+
+    cp(filePath, 'public/images/m1.png', function (err) {
+      console.log("err: ", err)
+      console.log("hello world")
+      //response.sendfile('public/images/m1.png')
+    });
+  }else {
+      cp(filePath, 'public/images/m2.png', function (err) {
+        console.log("err: ", err)
+        console.log("hello world")
+        //response.sendfile('public/images/m1.png')
+      });
+    }
+  }
   response.render('pages/profile');
 });
 
-app.post('/api/photo',function(request, response){
+
+app.post('/api/photo/player1',function(request, response){
   if(done==true){
-    console.log(request.files);
-    request.end(request.userPhoto.path);
+    //console.log(request.query.files);
+    console.log("filePath: ", filePath);
+    mv(filePath, 'public/images/m1.png', {mkdirp: true}, function(err){
+      console.log("hello world")
+      //response.sendfile('public/images/m1.png')
+    });
+    console.log("hello world2")
+    response.render('pages/profile')
+
   }
     //console.log(request.body);
     //response.end("" + request.body.name);
     //response.end()
 
 });
+
+app.post('/api/photo/player2',function(request, response){
+  if(done==true){
+    console.log(request.files);
+    console.log("filePath: ", filePath);
+    mv(filePath, 'public/images/m2.png', {mkdirp: true}, function(err){
+      console.log("hello world")
+      //response.sendfile('public/images/m1.png')
+    });
+    console.log("hello world2")
+    response.render('pages/profile')
+
+  }
+});
+
+app.get('/name',function(request, response){
+  var name = request.query.name;
+  response.render('pages/name',{
+    name: name
+  });
+  console.log(request.query.name);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
