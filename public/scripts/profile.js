04c34f73dce2f1fc2f8a811fa24db965301843dd
Julia Kyaw <skyblue.528@gmail.com>
2015-08-23 08:42:28 -0700

    Refactor profile page - include the players in the game page.

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,49 +1,4 @@
 $(document).ready(function() {
-    var selectedUrl = null;
-    $('.modal-content a').on('click', function(){
-        selectedUrl = ($(this).attr('href'));
-        $('.modal-content a').removeClass('selected');
-        $(this).addClass('selected');
-        console.log(selectedUrl);
-    })
-
-    $('#saveP1').on('click',function(){
-        console.log("selectedUrl: " +selectedUrl);
-
-        /* var hash = {
-         url :selectedUrl,
-         success : function(){
-         window.location.reload();
-         }
-         }  */
-
-        var hash = {} // hash map
-        hash.url = selectedUrl
-        hash.success = function(){
-            window.location.reload();
-        }
-        console.log("hello")
-
-        $.ajax(hash);
-    });
-
-    var selectedP2Url = null;
-    $('.modal-content a').on('click',function(){
-        selectedP2Url = $(this).attr('href')
-        $('.modal-content a').removeClass('selected');
-        $(this).addClass('selected');
-
-    })
-
-    $('#saveP2').on('click', function(){
-        $.ajax({
-            url: selectedP2Url,
-            success: function(){
-                window.location.reload();
-            }
-        })
-    });
-
     var avatars = [
         'hellokitty1.png', 'helloKitty2.png', 'helloKitty3.png', 'minion.png', 'minion2.png'
     ];
@@ -57,20 +12,34 @@ $(document).ready(function() {
             image_url: avatars[4]
         });
 
-        $('#players').append(html);
+        $('#player1').append(html);
         var html = template({
             player_number: 2,
             name: 'David',
             image_url: avatars[1]
         });
 
-        $('#players').append(html);
+        $('#player2').append(html);
     }
 
     if ($("#modal-template").length > 0) {
         var source = $("#modal-template").html();
         var template = Handlebars.compile(source);
         var html = template({});
-        $('#body').append(html);
+        $('body').append(html);
     }
+
+    // Clicking on select avatar should set the player number and show modal
+    var selectedPlayer = 1;
+    $('.select-avatar-btn').on('click', function() {
+        selectedPlayer = $(this).data('player-number');
+        $('#avatar-select-modal').modal();
+    });
+
+    // Select Avatar click should change player's image and close the modal
+    $('#avatar-select-modal a').on('click', function(){
+        var imageUrl = $(this).data('img');
+        $('#player' + selectedPlayer + ' img').attr('src', '/images/' + imageUrl);
+        $('#avatar-select-modal').modal('hide');
+    })
 });
--- tests/selenium/convert.js
+++ tests/selenium/convert.js
@@ -1,4 +1,10 @@
 var converter = require("selenium-html-js-converter");
+var fs = require('fs');
+var dir = 'tic-tac-toe';
+var files = fs.readdirSync(dir);
+
+console.log(files);
+
 var htmlFile = 'tic-tac-toe/blueWinsLeft.html';
 var exportFile = 'dist/blueWinsLeft.js';
 var testCaseName = 'BlueWinsLeft';
